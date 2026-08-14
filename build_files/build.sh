2c2ca2703c5abca77de90829df9f0f3e643872b6
JoaoSant7 <pedro_romano7@proton.me>
2026-08-14 07:49:25 -0300

    add terra repo

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,6 +8,16 @@ cp -avf "/ctx/system_files"/. /
 ### Enable COPR Repositories
 dnf5 -y copr enable lionheartp/Hyprland
 
+### Enable Terra Repo
+# Install the terra-release RPM first if it isn't pre-installed
+dnf5 install -y https://repos.fyra.labs/terra/terra-release.rpm || true
+
+# Safe sed execution (only run if file exists)
+if [ -f /etc/yum.repos.d/terra.repo ]; then
+  sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/terra.repo
+fi
+
+# remove bluefin unecessary apps
 dnf5 remove -y \
   input-remapper \
   nautilus \
@@ -20,6 +30,8 @@ dnf5 install -y \
 # hyprland
 dnf5 install -y \
   hyprland \
+  hyprutils \
+  nwg-look \
   qt5ct \
   qt6ct \
   xdg-desktop-portal-hyprland \
