50d4b136d9964e665b33a5449c0b0070a4521bf4
JoaoSant7 <pedro_romano7@proton.me>
2026-08-14 07:30:36 -0300

    add some pkgs and remove unecessary clutter

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,55 +2,50 @@
 
 set -ouex pipefail
 
-# Copy the contents of system_files/ of the git repo to /
+# Copy local overlay files to root /
 cp -avf "/ctx/system_files"/. /
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# Third-party repos
+### Enable COPR Repositories
 dnf5 -y copr enable lionheartp/Hyprland
 
-dnf remove -y \
-  thunderbird \
-  konsole \
-  kate
+dnf5 remove -y \
+  input-remapper \
+  nautilus \
+  ptyxis
 
-# this installs a package from fedora repos
-#
-# general apps
+# apps
 dnf5 install -y \
-  tmux \
-  kitty
+  thunar
 
+# hyprland
 dnf5 install -y \
   hyprland \
+  qt5ct \
+  qt6ct \
+  xdg-desktop-portal-hyprland \
+  xdg-desktop-portal-gtk \
   noctalia-git
 
-# === cli: Cli packages ===
-
+# cli
 dnf5 install -y \
+  eza \
+  tmux \
+  kitty \
   zoxide \
   zsh \
   htop \
-  nvim \
+  neovim \
   git \
   ripgrep \
   fzf \
-  fd \
+  fd-find \
   fastfetch
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+# remove unecessary clutter
+rm -f /usr/share/applications/documentation.desktop
+rm -f /usr/share/applications/discourse.desktop
 
 systemctl enable podman.socket
+
+# === cleanup ===
+dnf clean all && rm -rf /var/cache/dnf/*
