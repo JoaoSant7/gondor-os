86aed9a3aad562f6a03032f91c8e38e0852d22be
JoaoSant7 <pedro_romano7@proton.me>
2026-08-15 14:52:24 -0300

    remove a lot of gnome stuff

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,32 +22,6 @@ dnf5 install -y terra-release
 curl -fsSLo /etc/yum.repos.d/brave-browser.repo \
   https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
-# remove bluefin unecessary apps
-dnf5 remove -y \
-  input-remapper \
-  nautilus \
-  ptyxis \
-  rygel \
-  malcontent-control \
-  firewall-config
-
-# remove gnome desktop
-dnf5 remove -y \
-  gnome-shell \
-  gnome- extensions \
-  gnome-session \
-  gnome-tour \
-  gnome-system-monitor \
-  gdm
-
-# remove unecessary clutter
-rm -f /usr/share/applications/documentation.desktop
-rm -f /usr/share/applications/discourse.desktop
-rm -f /usr/share/applications/system-update.desktop
-rm -f /usr/share/applications/org.gnome.tweaks.desktop
-rm -f /usr/share/applications/gnome-background-panel.desktop
-rm -f /usr/share/applications/gnome-about-panel.desktop
-
 # apps
 dnf5 install -y \
   brave-origin \
@@ -99,6 +73,61 @@ dnf5 install -y \
   nwg-look \
   qt5ct \
   qt6ct
+
+# remove bluefin unecessary apps
+dnf5 remove -y \
+  gnome-color-manager \
+  input-remapper \
+  nautilus \
+  ptyxis \
+  rygel \
+  malcontent-control \
+  firewall-config \
+  nwg-panel \
+  fish
+
+# remove gnome desktop
+dnf5 remove -y \
+  gnome-disks \
+  gnome-shell \
+  gnome-extensions \
+  gnome-session \
+  gnome-tour \
+  gnome-system-monitor \
+  gnome-user-docs \
+  gdm \
+  gnome-shell-common \
+  mutter \
+  mutter-common \
+  gnome-settings-daemon \
+  gnome-control-center \
+  gnome-control-center-filesystem \
+  gnome-tweaks \
+  gnome-color-manager \
+  gnome-disk-utility \
+  gnome-backgrounds \
+  desktop-backgrounds-gnome \
+  f44-backgrounds-gnome \
+  gnome-user-docs \
+  gnome-app-list \
+  gnome-menus \
+  gnome-user-share \
+  gnome-remote-desktop \
+  gnome-epub-thumbnailer \
+  gnome-online-accounts \
+  gnome-online-accounts-libs \
+  gnome-bluetooth \
+  gnome-bluetooth-libs \
+  gweather-locations \
+  gweather-locations-common
+
+# remove unecessary clutter
+rm -f /usr/share/applications/documentation.desktop
+rm -f /usr/share/applications/discourse.desktop
+rm -f /usr/share/applications/system-update.desktop
+rm -f /usr/share/applications/org.gnome.tweaks.desktop
+rm -f /usr/share/applications/gnome-background-panel.desktop
+rm -f /usr/share/applications/gnome-about-panel.desktop
 
 sudo systemctl enable greetd
 sudo systemctl set-default graphical.target
