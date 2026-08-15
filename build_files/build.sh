2f92929d2f36c1494bbabb41e6d6a3367082e164
JoaoSant7 <pedro_romano7@proton.me>
2026-08-14 21:55:25 -0300

    fix import

--- build_files/build.sh
+++ build_files/build.sh
@@ -119,8 +119,7 @@ InputMethod=qtvirtualkeyboard
 EOF
 
 # Set custom wallpaper to sddm-astronaut-theme
-cp /ctx/assets/wallpapers/mountain-above-clouds.jpg \
-  /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/mountain-above-clouds.jpg
+cp /tmp/wallpaper.jpg /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/mountain-above-clouds.jpg
 
 sed -i 's|^Background=.*|Background="Backgrounds/mountain-above-clouds.jpg"|' \
   /usr/share/sddm/themes/sddm-astronaut-theme/Themes/astronaut.conf
