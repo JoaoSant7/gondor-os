b039f3e66359cc5f24e73be53daefdfe77737d44
JoaoSant7 <pedro_romano7@proton.me>
2026-08-16 07:20:35 -0300

    add greetd service

--- build_files/services.sh
+++ build_files/services.sh
@@ -6,6 +6,13 @@ set -ouex pipefail
 GREETD_UID=955
 GREETD_GID=955
 
+if ! grep -q "^greetd:" /usr/lib/passwd; then
+  echo "greetd:x:${GREETD_UID}:${GREETD_GID}:greetd daemon:/var/lib/greetd:/sbin/nologin" >>/usr/lib/passwd
+fi
+if ! grep -q "^greetd:" /usr/lib/group; then
+  echo "greetd:x:${GREETD_GID}:" >>/usr/lib/group
+fi
+
 # greetd home
 mkdir -p /var/lib/greetd
 chown -R ${GREETD_UID}:${GREETD_GID} /var/lib/greetd
