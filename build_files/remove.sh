796dc929f7934316fef773779b44c75f90feb200
JoaoSant7 <pedro_romano7@proton.me>
2026-08-17 19:37:39 -0300

    revamp remove build file

--- build_files/remove.sh
+++ build_files/remove.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-set -ouex pipefail
+#!/usr/bin/env bash
+set -oue pipefail
 
-# remove bluefin unecessary apps
+echo "Removing GNOME leftovers, input methods, and unused services..."
+
+# remove bluefin unnecessary apps
 dnf5 remove -y \
   input-remapper \
   nautilus \
+  nautilus-extensions \
+  orca \
   ptyxis \
   rygel \
   malcontent-control \
@@ -14,6 +18,7 @@ dnf5 remove -y \
 
 # remove gnome desktop
 dnf5 remove -y \
+  gnome-autoar \
   gnome-color-manager \
   gnome-disk-utility \
   gnome-shell \
@@ -44,7 +49,39 @@ dnf5 remove -y \
   gweather-locations \
   gweather-locations-common
 
-# remove unecessary clutter
+# remove ibus
+dnf5 remove -y \
+  ibus \
+  ibus-libs \
+  ibus-gtk2 \
+  ibus-gtk3 \
+  ibus-gtk4 \
+  ibus-panel \
+  ibus-setup \
+  ibus-typing-booster \
+  ibus-anthy \
+  ibus-hangul \
+  ibus-libpinyin \
+  ibus-m17n \
+  ibus-mozc \
+  ibus-unikey \
+  ibus-chewing
+
+# remove unnecessary clutter
+dnf5 remove -y \
+  gsound \
+  malcontent \
+  malcontent-ui-libs \
+  papers-libs \
+  papers-previewer \
+  papers-thumbnailer \
+  yelp-libs \
+  yelp-xsl
+
+# autoremove orphaned pkgs
+dnf5 autoremove -y || true
+
+# remove unnecessary clutter (desktop files)
 rm -f /usr/share/applications/documentation.desktop
 rm -f /usr/share/applications/discourse.desktop
 rm -f /usr/share/applications/system-update.desktop
